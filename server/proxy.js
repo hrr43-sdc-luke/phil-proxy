ac7d1eb209eaaa01614d99ce189edc6e095ea641
Phil <pronewgen92@gmail.com>
2020-02-28 18:27:44 -0500

    Set up proxy w/ next relic.

--- server/proxy.js
+++ server/proxy.js
@@ -1,11 +1,13 @@
+const nr = require('newrelic');
 const express = require('express');
 const ejs = require('ejs');
+const cors = require('cors')
 
 const port = process.env.PORT || 4000
 const app = express();
 
-
 app.use(express.static('./public'));
+app.use(cors())
 app.engine('html', ejs.renderFile);
 
 app.get('/:id', (req, res) => {
